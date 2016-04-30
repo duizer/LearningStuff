35f796e01e84f6133359fb29198feb89cf1e677f
Paul Toft Duizer <ptd@trifork.com>
2016-04-30 17:25:16 +0200

    Website added

---
+++ LearningStuff/Controllers/HelloController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace LearningStuff.Controllers
+{
+    public class HelloController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
